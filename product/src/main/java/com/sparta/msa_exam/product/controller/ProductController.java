6b0bbe9447ac393831dbe1551f71c9a335b2f805
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-08-07 23:12:32 +0900

    feat: add logs

--- product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
+++ product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -4,12 +4,14 @@ import com.sparta.msa_exam.product.dto.ProductRequestDto;
 import com.sparta.msa_exam.product.entity.Product;
 import com.sparta.msa_exam.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
@@ -19,12 +21,17 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
-        return ResponseEntity.ok().body(productService.findAll());
+        log.info("Received request to get all products.");
+        List<Product> products = productService.findAll();
+        log.info("Returning {} products.", products.size());
+        return ResponseEntity.ok().body(products);
     }
 
     @PostMapping
     public ResponseEntity<Void> addProduct(@RequestBody ProductRequestDto productRequestDto) {
+        log.info("Received request to add a new product: {}", productRequestDto);
         productService.addProduct(productRequestDto);
+        log.info("Product added successfully.");
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
