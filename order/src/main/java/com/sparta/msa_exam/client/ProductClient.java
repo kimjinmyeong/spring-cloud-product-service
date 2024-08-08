462dd836ef0693cf59d2d2745fb7244d58c83283
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-08-08 17:41:14 +0900

    fix: modify uri product to products

--- order/src/main/java/com/sparta/msa_exam/client/ProductClient.java
+++ order/src/main/java/com/sparta/msa_exam/client/ProductClient.java
@@ -2,12 +2,14 @@ package com.sparta.msa_exam.client;
 
 import com.sparta.msa_exam.dto.ProductDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
 @FeignClient(name = "product")
 public interface ProductClient {
-    @GetMapping("/product")
-    List<ProductDto> getProducts();
+    @GetMapping("/products")
+    ResponseEntity<List<ProductDto>> getProducts(@RequestHeader("Authorization") String token);
 }
